306cf4d21d2d4b7512b7edfb08c8099b549eaefd
Stephen Badger <stephen.badger@vitalbeats.com>
2020-03-25 12:16:11 +0000

    Explicitly set EKS version
    This is required so that we can upgrade EKS versions in future module 
    releases.

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -36,6 +36,7 @@ resource "aws_iam_role_policy_attachment" "vb-cluster-AmazonEKSServicePolicy" {
 resource "aws_eks_cluster" "vb" {
   name     = var.cluster-name
   role_arn = aws_iam_role.vb-cluster.arn
+  version  = "1.15"
 
   vpc_config {
     security_group_ids = [aws_security_group.vb-cluster.id]
