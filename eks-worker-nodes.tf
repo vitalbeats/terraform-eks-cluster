effd1de062c9881aff366ffe0e81bb5bb2c9933b
Stephen Badger <stephen.badger@vitalbeats.com>
2020-03-30 17:02:31 +0100

    Reduce default scaling size

--- eks-worker-nodes.tf
+++ eks-worker-nodes.tf
@@ -45,7 +45,7 @@ resource "aws_eks_node_group" "ng-workers" {
   instance_types  = ["t3.xlarge"]
 
   scaling_config {
-    desired_size = 2
+    desired_size = 1
     max_size     = 3
     min_size     = 1
   }
