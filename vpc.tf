686fbd0f70ee1738c99316602c8a6269e4e0895b
Stephen Badger <stephen.badger@vitalbeats.com>
2020-03-30 17:04:37 +0100

    Take two are public-only node setup

--- vpc.tf
+++ vpc.tf
@@ -76,36 +76,3 @@ resource "aws_route_table_association" "vb-public" {
   subnet_id      = aws_subnet.vb-public.*.id[count.index]
   route_table_id = aws_route_table.vb-public.id
 }
-
-# NAT Gateway to allow traffic from private subnets to Internet via NAT gateway
-resource "aws_eip" "nat" {
-  depends_on = [aws_internet_gateway.vb]
-  vpc        = true
-}
-
-resource "aws_nat_gateway" "vb" {
-  depends_on    = [aws_internet_gateway.vb]
-  subnet_id     = aws_subnet.vb-public[0].id
-  allocation_id = aws_eip.nat.id
-}
-
-# Routing table for private subnets
-resource "aws_route_table" "vb-private" {
-  vpc_id = aws_vpc.vb.id
-  tags = {
-    Name = "${var.cluster-name}/PrivateRouteTable"
-  }
-}
-
-resource "aws_route_table_association" "vb-private" {
-  count = 3
-
-  subnet_id      = aws_subnet.vb-private.*.id[count.index]
-  route_table_id = aws_route_table.vb-private.id
-}
-
-resource "aws_route" "private_route" {
-  route_table_id         = aws_route_table.vb-private.id
-  destination_cidr_block = "0.0.0.0/0"
-  nat_gateway_id         = aws_nat_gateway.vb.id
-}
