c66433f3ce2176ae6f466c698f1a19010227f88c
Stephen Badger <stephen.badger@vitalbeats.com>
2020-12-02 15:59:58 +0000

    Include basic datadog monitor

---
+++ datadog.tf
@@ -0,0 +1,15 @@
+resource "datadog_monitor" "datadog-up" {
+  name               = "DataDog is sending data"
+  type               = "service check"
+  message            = "DataDog has stopped sending data. Notify: @devops"
+
+  query = "datadog.agent.up.over(cluster_name:${var.cluster-name}).last(5).count_by_status()"
+
+  notify_no_data    = true
+
+  lifecycle {
+    ignore_changes = [silenced]
+  }
+
+  tags = ["cluster_name:${var.cluster-name}"]
+}
--- main.tf
+++ main.tf
@@ -3,5 +3,6 @@ terraform {
       aws = ">= 2.62.0"
       local = "~> 1.4"
       null = "~> 2.1"
+      datadog = ">= 2.17.0"
   }
 }
