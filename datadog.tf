647b47f0c1d70d41ec9274348201b733f747339e
Stephen Badger <stephen.badger@vitalbeats.com>
2020-12-02 16:15:25 +0000

    Fix basic datadog monitor

--- datadog.tf
+++ datadog.tf
@@ -1,9 +1,9 @@
 resource "datadog_monitor" "datadog-up" {
-  name               = "DataDog is sending data"
+  name               = "DataDog is sending data for ${var.cluster-name}"
   type               = "service check"
   message            = "DataDog has stopped sending data. Notify: @devops"
 
-  query = "datadog.agent.up.over(cluster_name:${var.cluster-name}).last(5).count_by_status()"
+  query = "'datadog.agent.up'.over(cluster_name:${var.cluster-name}).last(5).count_by_status()"
 
   notify_no_data    = true
 
