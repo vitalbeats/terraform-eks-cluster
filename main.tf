f64521453ab819e2a4afeb264b79880a522f9f32
Stephen Badger <stephen.badger@vitalbeats.com>
2020-12-14 09:43:12 +0000

    Upgrade to Terraform 0.13+

--- main.tf
+++ main.tf
@@ -1,8 +1,20 @@
 terraform {
   required_providers {
-      aws = ">= 2.62.0"
-      local = "~> 1.4"
-      null = "~> 2.1"
-      datadog = ">= 2.17.0"
+      aws = {
+        source = "hashicorp/aws"
+        version = ">= 2.62.0"
+      }
+      local = {
+        source = "hashicorp/local"
+        version = "~> 1.4"
+      }
+      null = {
+        source = "hashicorp/null"
+        version = "~> 2.1"
+      }
+      datadog = {
+        source = "DataDog/datadog"
+        version = ">= 2.17.0"
+      }
   }
 }
