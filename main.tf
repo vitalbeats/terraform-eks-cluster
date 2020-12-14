501852c40018d630ea234616b329898beda50c6c
Stephen Badger <stephen.badger@vitalbeats.com>
2020-12-14 09:48:41 +0000

    Fix required provider naming

--- main.tf
+++ main.tf
@@ -1,20 +1,20 @@
 terraform {
   required_providers {
-      aws = {
-        source = "hashicorp/aws"
-        version = ">= 2.62.0"
-      }
-      local = {
-        source = "hashicorp/local"
-        version = "~> 1.4"
-      }
-      null = {
-        source = "hashicorp/null"
-        version = "~> 2.1"
-      }
-      datadog = {
-        source = "DataDog/datadog"
-        version = ">= 2.17.0"
-      }
+    aws = {
+      source = "hashicorp/aws"
+      version = ">= 2.62.0"
+    }
+    local = {
+      source = "hashicorp/local"
+      version = "~> 1.4"
+    }
+    null = {
+      source = "hashicorp/null"
+      version = "~> 2.1"
+    }
+    datadog = {
+      source = "DataDog/datadog"
+      version = ">= 2.17.0"
+    }
   }
 }
